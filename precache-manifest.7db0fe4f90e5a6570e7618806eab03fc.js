cf3f45f1c493d7d392bc94c453b706a50a9efb9c
Kevin Hazelton <kshazelton@gmail.com>
2022-03-22 16:08:52 -0400

    Initial website design. Contact form doesn't work yet.

---
+++ precache-manifest.7db0fe4f90e5a6570e7618806eab03fc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3675c7b9363e405c9478a920332a9e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce626a6eddeaa081079b",
+    "url": "/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "430306718396512a7939",
+    "url": "/static/css/main.f4f461db.chunk.css"
+  },
+  {
+    "revision": "ce626a6eddeaa081079b",
+    "url": "/static/js/2.62e30fac.chunk.js"
+  },
+  {
+    "revision": "36cc6488da78fb1c20c2a22e52754a0b",
+    "url": "/static/js/2.62e30fac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "430306718396512a7939",
+    "url": "/static/js/main.1e0a4642.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "8051989923efb838184a3a988efc2a0e",
+    "url": "/static/media/coverphoto.80519899.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  }
+]);
